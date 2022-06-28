094aae5ad3ef473b22dc9d44b467baf1d2096ac1
Shahid Ullah <shahidullah@xtremessoft.com>
2022-06-28 16:49:15 +0500

    Project Dashboard - Changes

--- WebApp/Scripts/FunctionalityJs/Areas/Project/Dashboard/project-dashboard.js
+++ WebApp/Scripts/FunctionalityJs/Areas/Project/Dashboard/project-dashboard.js
@@ -1,11 +1,13 @@
-﻿
+﻿var loggedInUser = {};
 $(function () {
+
+    loggedInUser = JSON.parse(localStorage.getItem('User'));
     ajaxRequest({
         commandName: 'Project_Dashboard_CountByStatus_Get', values:
         {
-            EmployeeId:0,
-            UserId:0,    
-            Role: 0,            
+            EmployeeId: loggedInUser.employeeId,
+            UserId: loggedInUser.id,
+            Role: loggedInUser.roleId,
             Language: _currentLanguage
         }, CallBack: getProjectCountByVIPUrgentCallBack
     });
@@ -13,11 +15,14 @@ $(function () {
     getClientMeetingContractorCount();
     getIssueCountByStatus();
     getProjectCountByStatus();
+
+    //renderKendoChart();
 });
+
 var getProjectCountByVIPUrgentCallBack = function (responseJSON) {
     prepareChartDataFor_ProjectCountByVIPUrgent(JSON.parse(responseJSON.Value));
 }
-function prepareChartDataFor_ProjectCountByVIPUrgent(inputJSON){
+function prepareChartDataFor_ProjectCountByVIPUrgent(inputJSON) {
     var lables = ["All", "Urgent", "Non-Urgent", "VIP", "Non-VIP"];
     var data = [];
     data.push(inputJSON.all);
@@ -25,40 +30,42 @@ function prepareChartDataFor_ProjectCountByVIPUrgent(inputJSON){
     data.push(inputJSON.noUrgent);
     data.push(inputJSON.vip);
     data.push(inputJSON.nonVIP);
-    renderBarChart('chart-project-count-by-vip-urgent', 'Project By VIP/Urgent', lables, data);
+    //renderBarChart('chart-project-count-by-vip-urgent', lblProjectByVIPUrgent, lables, data);
+    renderKendoChart('chart-project-count-by-vip-urgent-kendo', data, lables, lblProjectByVIPUrgent)
 }
-function getProejctCountByCategory() {    
+function getProejctCountByCategory() {
     ajaxRequest({
         commandName: 'Project_Dashboard_CountByCategory_Get', values:
         {
-            EmployeeId: 0,
-            UserId: 0,
-            Role: 0,
+            EmployeeId: loggedInUser.employeeId,
+            UserId: loggedInUser.id,
+            Role: loggedInUser.roleId,
             Language: _currentLanguage
         }, CallBack: getProejctCountByCategoryCallBack
     });
 }
-var getProejctCountByCategoryCallBack = function (responseJSON) {    
+var getProejctCountByCategoryCallBack = function (responseJSON) {
     prepareChartDataFor_ProjectCountByCategory(JSON.parse(responseJSON.Value));
 }
 function prepareChartDataFor_ProjectCountByCategory(inputJSON) {
     var labels = [];
     var data = [];
-    $.each(inputJSON, function (index, value) {        
+    $.each(inputJSON, function (index, value) {
         labels.push(value.categoryType);
         data.push(value.count);
     });
-    renderBarChart('chart-project-count-by-category', 'Project By Category', labels, data);
+    renderBarChart('chart-project-count-by-category', lblProjectByCategory, labels, data);
+    renderKendoChart('chart-project-count-by-category-kendo', data, labels, lblProjectByCategory);
 
-    
+
 }
 function getClientMeetingContractorCount() {
     ajaxRequest({
         commandName: 'Project_Dashboard_ClientMeetingContractorCount_Get', values:
         {
-            EmployeeId: 0,
-            UserId: 0,
-            Role: 0,
+            EmployeeId: loggedInUser.employeeId,
+            UserId: loggedInUser.id,
+            Role: loggedInUser.roleId,
             Language: _currentLanguage
         }, CallBack: getClientMeetingContractorCountCallBack
     });
@@ -70,21 +77,22 @@ var getClientMeetingContractorCountCallBack = function (responseJSON) {
     $('#TotalContractor').text(JSON.parse(responseJSON.Value).contractors);
 }
 function prepareChartDataFor_ClientMeetingContractorCount(inputJSON) {
-    var labels = ['Clients','Meetings', 'Contractors'];
+    var labels = ['Clients', 'Meetings', 'Contractors'];
     var data = [];
     data.push(inputJSON.clients)
     data.push(inputJSON.meetings)
     data.push(inputJSON.contractors)
-    
-    renderBarChart('chart-total-client-meeting-contractor', 'Client, Meeting & Contractor', labels, data);
+
+    renderBarChart('chart-total-client-meeting-contractor', lblClientMeetingAndContractor, labels, data);
+    renderKendoChart('chart-total-client-meeting-contractor-kendo', data, labels, lblClientMeetingAndContractor);
 }
 function getIssueCountByStatus() {
     ajaxRequest({
         commandName: 'Project_Dashboard_IssueCountByStatus_Get', values:
         {
-            EmployeeId: 0,
-            UserId: 0,
-            Role: 0,
+            EmployeeId: loggedInUser.employeeId,
+            UserId: loggedInUser.id,
+            Role: loggedInUser.roleId,
             Language: _currentLanguage
         }, CallBack: getIssueCountByStatusCallBack
     });
@@ -92,7 +100,7 @@ function getIssueCountByStatus() {
 var getIssueCountByStatusCallBack = function (responseJSON) {
     prepareChartDataFor_IssueCountByStatus(JSON.parse(responseJSON.Value));
 }
-function prepareChartDataFor_IssueCountByStatus(inputJSON) {    
+function prepareChartDataFor_IssueCountByStatus(inputJSON) {
     var labels = [];
     var data = [];
     var allCount = 0;
@@ -101,11 +109,12 @@ function prepareChartDataFor_IssueCountByStatus(inputJSON) {
     });
     labels.push('All');
     data.push(allCount);
-    $.each(inputJSON, function (index, value) {        
+    $.each(inputJSON, function (index, value) {
         labels.push(value.status);
         data.push(value.count);
     });
-    renderBarChart('chart-issue-count-by-status', 'Issue By Status', labels, data);
+    renderBarChart('chart-issue-count-by-status', lblIssueByStatus, labels, data);
+    renderKendoChart('chart-issue-count-by-status-kendo', data, labels, lblIssueByStatus);
 
 
 }
@@ -114,9 +123,9 @@ function getProjectCountByStatus() {
     ajaxRequest({
         commandName: 'Project_Dashboard_ProjectCountByStatus_Get', values:
         {
-            EmployeeId: 0,
-            UserId: 0,
-            Role: 0,
+            EmployeeId: loggedInUser.employeeId,
+            UserId: loggedInUser.id,
+            Role: loggedInUser.roleId,
             Language: _currentLanguage
         }, CallBack: getProjectCountByStatusCallBack
     });
@@ -124,7 +133,7 @@ function getProjectCountByStatus() {
 var getProjectCountByStatusCallBack = function (responseJSON) {
     prepareChartDataFor_ProjectCountByStatus(JSON.parse(responseJSON.Value));
 }
-function prepareChartDataFor_ProjectCountByStatus(inputJSON) {    
+function prepareChartDataFor_ProjectCountByStatus(inputJSON) {
     var labels = [];
     var data = [];
     var allCount = 0;
@@ -137,7 +146,8 @@ function prepareChartDataFor_ProjectCountByStatus(inputJSON) {
         labels.push(value.status);
         data.push(value.count);
     });
-    renderBarChart('chart-project-count-by-status', 'Project By Status', labels, data);
+    renderBarChart('chart-project-count-by-status', lblProjectByStatus, labels, data);
+    renderKendoChart('chart-project-count-by-status-kendo', data, labels, lblProjectByStatus);
 
 
 }
@@ -205,3 +215,94 @@ function renderBarChart(divID, chartTitle, lables, data) {
         }
     });
 }
+
+
+/* Set chart container height */
+//var data = [560, 630, 740, 910, 1170, 1380, 100, 150, 100, 100, 300];
+//var chartHeight = data.length * 50;
+//$("#chart").css("height", chartHeight);
+function renderKendoChart(divID, data, categories, chartTitle) {
+    $("#" + divID).kendoChart({
+        title: {
+            text: chartTitle
+        },        
+        legend: {
+            visible: true
+        },
+        seriesDefaults: {
+            type: "column"
+        },
+        series: [{
+            data: data,
+            tooltip: {
+                position: "inside"
+            },
+            color: function (point) {
+                
+                if (point.index == 0) {
+                    return "rgb(255, 99, 88)";
+                }
+                else if (point.index == 1) {
+                    return "rgb(255, 210, 70)";
+                }
+                else if (point.index == 2) {
+                    return "rgb(120, 210, 55)";
+                }
+                else if (point.index == 3) {
+                    return "rgb(40, 180, 200)";
+                }
+                else if (point.index == 4) {
+                    return "rgb(255, 99, 88)";
+                }
+                else if (point.index == 5) {
+                    return "rgb(255, 210, 70)";
+                }
+                else if (point.index == 6) {
+                    return "rgb(120, 210, 55)";
+                }
+                else if (point.index == 7) {
+                    return "rgb(40, 180, 200)";
+                }
+                else if (point.index == 8) {
+                    return "rgb(255, 99, 88)";
+                }
+                else if (point.index == 9) {
+                    return "rgb(255, 210, 70)";
+                }
+                else if (point.index == 10) {
+                    return "rgb(120, 210, 55)";
+                }
+                else if (point.index == 11) {
+                    return "rgb(40, 180, 200)";
+                }
+
+                // use the default series theme color
+            }
+        }],
+        valueAxis: {
+            //max: 400,
+            line: {
+                visible: false
+            },
+            minorGridLines: {
+                visible: true
+            },
+            labels: {
+                rotation: "auto"                
+            }
+        },
+        categoryAxis: {
+            categories: categories, //["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+            majorGridLines: {
+                visible: false
+            }
+        },
+        tooltip: {
+            visible: true,
+            template: "#= value #",
+        }
+    });
+}
+
+//$(document).ready(renderKendoChart);
+//$(document).bind("kendo:skinChange", renderKendoChart);
